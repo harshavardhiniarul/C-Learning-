508f8e482d2f1f1b028386c38bb188801ca08ce4
harshavardhiniarul <harshavardhiniarul568@gmail.com>
2026-03-06 20:30:27 +0530

    C basics

---
+++ C program/mar5.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b, *p, *q, sum;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    p = &a;
+    q = &b;
+
+    sum = *p + *q;
+
+    printf("Sum = %d", sum);
+
+    return 0;
+}
